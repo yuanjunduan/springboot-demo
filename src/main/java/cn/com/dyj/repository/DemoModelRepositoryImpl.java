0b6c621519f55f2d850f751a20d002bf46540e5d
duanyuanjun <duanyuanjun@glzc.com>
2017-10-25 15:01:00 +0800

    自定义sql

---
+++ src/main/java/cn/com/dyj/repository/DemoModelRepositoryImpl.java
@@ -0,0 +1,27 @@
+package cn.com.dyj.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ * duanyuanjun 2017/10/25 12:51
+ */
+@Repository
+public class DemoModelRepositoryImpl {
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  //自定义sql
+  public List<Object> excuteSql(String sql) {
+
+    final Query query = entityManager.createQuery(sql);
+    return query.getResultList();
+
+  }
+
+}
